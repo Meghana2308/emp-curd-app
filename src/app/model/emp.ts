c828e8112d8c40e0feb2db5c9b838fb8170cbf21
Meghana <meghanadamerla2308@gmail.com>
2020-02-26 00:33:59 -0800

    emp-curd-app

---
+++ src/app/model/emp.ts
@@ -0,0 +1,11 @@
+export class Emp {
+    public empId:number;
+    public firstName:string;
+    public lastName:string;
+    public basic:number;
+    public joinDate:Date;
+    public dept:string;
+    public mobileNumber:string;
+    public email:string;
+
+}
